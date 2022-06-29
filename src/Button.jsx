0a84fd097c3b3a2bb7983934adadd20fd8a93921
jo4ovictor <joaovictordeaguiarmoura@gmail.com>
2022-06-29 14:46:50 -0400

    criando projeto

---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+import "./Button.css";
+function Button({frase}) {
+
+    return(
+        <button className="maisNoticias">
+        {frase}
+        </button>
+    ); 
+};
+export default Button;
